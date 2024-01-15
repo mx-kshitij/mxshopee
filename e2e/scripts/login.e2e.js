289728b769e7753c803cc8adbb99a161b53039c5
Kshitij Karandikar <kshitij.karandikar@mendix.com>
2024-01-15 12:03:56 +0100

    Updated code to disable sync before login action.

--- e2e/scripts/login.e2e.js
+++ e2e/scripts/login.e2e.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('Example login', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -13,13 +13,16 @@ describe('Example', () => {
     
     await waitFor(element(by.id('container11'))).toBeVisible().withTimeout(10000);
     await element(by.id('container11')).tap();
+    
     await element(by.id('textBox3')).typeText('detox');
     await element(by.id('textBox4')).typeText('DetoxWins123!');
-    console.log('before');
+    
+    await device.disableSynchronization()
     await element(by.id('actionButton2')).tap();
-    console.log('after');
+    
 
-    // await waitFor(element(by.id('text16$buttonDone'))).toBeVisible().withTimeout(10000);
-    await expect(element(by.id('text16'))).toBeVisible();
+    // await device.enableSynchronization();
+    await waitFor(element(by.id('text16'))).toBeVisible().withTimeout(10000);
+    // await expect(element(by.id('text16'))).toBeVisible();
   });
 });
